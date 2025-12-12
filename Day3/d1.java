9797f781a45477dbc24068a82a306875e50afe86
praveen-23becse156 <mpraveenm56@gmail.com>
2025-12-12 14:01:36 +0530

    day3 changes

---
+++ Day3/d1.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class d1 {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        if(a>b)
+        {
+          System.out.println(a);
+        }
+        else
+        {
+          System.out.println(b);
+        }
+
+    }
+  }  
---
+++ Day3/d2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class d2 {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        if(b>a)
+        {
+          System.out.println(a);
+        }
+        else
+        {
+          System.out.println(b);
+        }
+
+    }
+  }
